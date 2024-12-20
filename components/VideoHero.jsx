5f1ad432db72edada912faba4e67364afba9d48b
Emanuel Dario Garcia <emanueldario.dev@gmail.com>
2024-12-20 22:26:43 +0100

    whatsapp icon added

--- components/VideoHero.jsx
+++ components/VideoHero.jsx
@@ -7,7 +7,7 @@ const VideoHero = () => {
                 src='cptVideo.mp4' 
                 className='absolute object-cover -z-10 w-full'
                 autoPlay='{true}' loop muted
-            />s
+            />
             <div className='w-full h-full z-20 flex flex-col justify-center items-center font-[Dela-Gothic-One] text-white font-semibold'>
                 <h1 className='text-[90px] '>CUMBIA</h1>
                 <p className='text-[85px]'>PARA TODXS</p>
--- components/WhatsAppApi.tsx
+++ components/WhatsAppApi.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FaWhatsapp } from "react-icons/fa"
 
 const WhatsAppApi = () => {
   return (
-    <div>WhatsAppApi</div>
-  )
+    <div className='flex'>
+        <a href="https://wa.me/34634017750" className='btn-wsp' target='blank'>
+            <FaWhatsapp />
+        </a>
+    </div>  )
 }
 
 export default WhatsAppApi
