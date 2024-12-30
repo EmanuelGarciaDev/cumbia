cd08191eef050c9c4e35a5bfec755145a4cbfc58
Emanuel Dario Garcia <emanueldario.dev@gmail.com>
2024-12-30 13:15:42 +0100

    youtube width is 100% - media is now fully responsive

--- components/Media.tsx
+++ components/Media.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import InstagramApi from './InstagramApi'
 import SpotifyApi from './SpotifyApi'
-import { Group, Stack } from '@mantine/core'
+import { Group, Stack, Title } from '@mantine/core'
 import TikTok from './TikTok'
 import YoutubeApi from './YoutubeApi'
 
 const Media = () => {
-  return (
-    <div className="bg-slate-200x p-8">
-      <Group gap="lg" justify="center" className='flex items-start'>
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
+
+  useEffect(() => {
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+    
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);  return (
+    <div className="bg-slate-800 p-2">
+      <Title order={1} className='text-slate-200 pb-4 flex justify-center'>Seguinos en nuestras redes </Title>
+      { isMobile ? (
+      <Stack gap="xs" align="stretch" justify="flex-start">
           <InstagramApi />
           <TikTok />
-          <Stack>
-            <SpotifyApi />
-            <YoutubeApi />
-          </Stack>
-      </Group>
+          <SpotifyApi />
+          <YoutubeApi />
+      </Stack>
+          ) : (
+          <Group gap="" justify="center" className='flex items-start'>
+              <InstagramApi />
+              <TikTok />
+            <Stack>
+              <YoutubeApi />
+              <SpotifyApi />
+            </Stack>
+          </Group>
+          )
+          }
     </div>
   )
 }
--- components/YoutubeApi.tsx
+++ components/YoutubeApi.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const YoutubeApi = () => {
   return (
     <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/TKE8ZI68VCc?si=ZUuZE0pIzYxdJLHu" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/TKE8ZI68VCc?si=ZUuZE0pIzYxdJLHu" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   )
 }
